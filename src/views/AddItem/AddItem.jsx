66582b90003b351a27bc8bbe9957588089e5c56e
Natasha <njohnson533@gmail.com>
2022-04-12 13:18:14 -0400

    implements add item funtionality and list view

--- src/views/AddItem/AddItem.jsx
+++ src/views/AddItem/AddItem.jsx
@@ -1,3 +1,92 @@
+import React, { useState } from 'react';
+import { db } from '../../lib/firebase';
+import { collection, addDoc } from 'firebase/firestore';
+
 export default function AddItem() {
-  return <div>This is the Add Item view</div>;
+  const [listItem, setListItem] = useState({
+    name: '',
+    frequency: '7',
+    date: '',
+  });
+
+  const handleInput = (e) => {
+    const { name, value } = e.target;
+
+    setListItem({
+      ...listItem,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addItem();
+  };
+
+  const addItem = async () => {
+    try {
+      const docRef = await addDoc(collection(db, 'list'), {
+        property: listItem,
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  const { name, frequency, date } = listItem;
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="itemName">Item Name</label>
+      <input
+        type="text"
+        htmlFor="name"
+        id="name"
+        onChange={handleInput}
+        value={name}
+        name="name"
+      />
+      <fieldset>
+        <legend>Frequency</legend>
+        <label htmlFor="7">Soon</label>
+        <input
+          type="radio"
+          name="frequency"
+          value="7"
+          id="7"
+          checked={frequency === '7'}
+          onChange={handleInput}
+        />
+        <label htmlFor="14">Kind of Soon</label>
+        <input
+          type="radio"
+          name="frequency"
+          value="14"
+          id="14"
+          checked={frequency === '14'}
+          onChange={handleInput}
+        />
+        <label htmlFor="30">Not Soon</label>
+        <input
+          type="radio"
+          name="frequency"
+          value="30"
+          id="30"
+          checked={frequency === '30'}
+          onChange={handleInput}
+        />
+      </fieldset>
+      <label htmlFor="30" id="lastPurchase">
+        Date of Last Purchase
+      </label>
+      <input
+        type="date"
+        id="lastPurchase"
+        onChange={handleInput}
+        value={date}
+        name="date"
+      />
+      <button type="submit">Add Item</button>
+    </form>
+  );
 }
--- src/views/List/List.jsx
+++ src/views/List/List.jsx
@@ -1,3 +1,27 @@
+import React, { useState, useEffect } from 'react';
+import { collection, onSnapshot } from 'firebase/firestore';
+import { db } from '../../lib/firebase';
+
 export default function List() {
-  return <div>This is the list view</div>;
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const unsubscribe = onSnapshot(collection(db, 'list'), (snapshot) => {
+      const snapshotDocs = [];
+      snapshot.forEach((doc) => snapshotDocs.push(doc.data().property));
+      setData(snapshotDocs);
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  return (
+    <ul>
+      {data.map((listItem, index) => {
+        const { name } = listItem;
+        return <li key={index}>{name}</li>;
+      })}
+    </ul>
+  );
 }
