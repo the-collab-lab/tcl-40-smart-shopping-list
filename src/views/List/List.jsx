8f25f2b7043b79fbd5a8d005184eabdf899c746b
k-schrattenholzer <ktschr11@gmail.com>
2022-04-29 08:53:58 -0700

    adds update to firebase item on check, and useEffect to uncheck item automatically

--- src/views/List/List.jsx
+++ src/views/List/List.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { collection, onSnapshot } from 'firebase/firestore';
+import { collection, doc, onSnapshot, updateDoc } from 'firebase/firestore';
 import { db } from '../../lib/firebase';
 
 export default function List({ token }) {
@@ -20,19 +20,44 @@ export default function List({ token }) {
     };
   }, [token]);
 
-  const onChange = (listItem) => {
+  useEffect(() => {
+    data.forEach((item) => {
+      const now = Date.now();
+      const delta = now - item.lastPurchasedAt;
+      // console.log(item.name,'now', now)
+      // console.log(item.name,'delta', delta)
+      unCheckItem(item, delta);
+    });
+  });
+
+  const unCheckItem = async (item, delta) => {
+    if (delta > 86400000) {
+      item.isActive = false;
+      const updatedItem = doc(db, token, item.id);
+      await updateDoc(updatedItem, {
+        isActive: false,
+      });
+    }
+  };
+
+  const onChange = async (listItem) => {
     const { isActive, id } = listItem;
     // this will represent the last purchased date rather than the current time
-    const now = Date.now();
-    const lastPurchasedAtInMS = Date.now() - 86400000;
-    const delta = now - lastPurchasedAtInMS;
-    const nextData = data.map((doc) => {
-      if (doc.id === id) {
-        doc.isActive = !doc.isActive;
+    // const now = Date.now();
+    // const lastPurchasedAtInMS = Date.now() - 86400000;
+    // const delta = now - lastPurchasedAtInMS;
+    const nextData = data.map((item) => {
+      if (item.id === id) {
+        item.isActive = !item.isActive;
       }
-      return doc;
+      return item;
     });
     setData(nextData);
+    const updatedDoc = doc(db, token, listItem.id);
+    await updateDoc(updatedDoc, {
+      isActive: !isActive,
+      lastPurchasedAt: Date.now(),
+    });
   };
 
   return (
