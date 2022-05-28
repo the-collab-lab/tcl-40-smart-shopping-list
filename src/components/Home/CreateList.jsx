55f0904b1869eebee49f78ea27dbbae2c0442e81
Sonja Parsell <sonjaparsell09@gmail.com>
2022-05-28 12:19:24 -0400

    added Home.css, App.css and button class

--- src/components/Home/CreateList.jsx
+++ src/components/Home/CreateList.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import '../../App.css';
+import './Home.css';
 
 const CreateList = (props) => {
   return (
     <div>
       <h3>Welcome to your Smart Shopping list!</h3>
-      <button onClick={props.newToken}>Create a new token</button>
+      <button className="createButton" onClick={props.newToken}>
+        Create a new token
+      </button>
     </div>
   );
 };
