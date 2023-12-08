6a726d1b190156a6d40a7f222dd705534719418d
itsoliviasparks <itsoliviasparks@gmail.com>
2023-12-07 17:43:37 -0700

    update database info

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -4,12 +4,12 @@ import { getFirestore } from 'firebase/firestore';
 
 // Initialize Firebase.
 const firebaseConfig = {
-  apiKey: 'AIzaSyCEGx9eyTmlvNXUxZRnMw6BLy_w0ikmOxM',
-  authDomain: 'tcl-40-smart-shopping-li-23b8c.firebaseapp.com',
-  projectId: 'tcl-40-smart-shopping-li-23b8c',
-  storageBucket: 'tcl-40-smart-shopping-li-23b8c.appspot.com',
-  messagingSenderId: '72982113492',
-  appId: '1:72982113492:web:06cdf136544ddd4d89df41',
+  apiKey: 'AIzaSyAap0h-QvB72HyPy190OqyxE-afDVlLRQk',
+  authDomain: 'tcl-40-smart-shopping-list.firebaseapp.com',
+  projectId: 'tcl-40-smart-shopping-list',
+  storageBucket: 'tcl-40-smart-shopping-list.appspot.com',
+  messagingSenderId: '1091305950811',
+  appId: '1:1091305950811:web:7395029344d96ae759f97e',
 };
 
 export const app = initializeApp(firebaseConfig);
